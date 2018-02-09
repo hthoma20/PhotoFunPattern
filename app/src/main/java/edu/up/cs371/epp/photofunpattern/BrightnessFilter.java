2355266440941e4cc77e9b09a41d5921f53104b4
snook20 <snook20@up.edu>
2018-02-08 17:33:21 -0800

    dag

--- app/src/main/java/edu/up/cs371/epp/photofunpattern/BrightnessFilter.java
+++ app/src/main/java/edu/up/cs371/epp/photofunpattern/BrightnessFilter.java
@@ -23,12 +23,11 @@ public class BrightnessFilter extends PhotoFilter {
     * @param inPixel is a 32 bit pixel that contains RGB color values
     * @return a new Pixel in which each of the RGB components has been increased
     */
-    public int transformPixel(int inPixel) {
-        int red = constrain(Color.red(inPixel) + ADJUSTMENT);
-        int green = constrain(Color.green(inPixel) + ADJUSTMENT);
-        int blue = constrain(Color.blue(inPixel) + ADJUSTMENT);
-        int outPixel = Color.argb(Color.alpha(inPixel), red, green, blue);
-        return outPixel;
+    public int transformPixel(int p0, int p1, int p2, int p3, int p4, int p5, int p6, int p7, int p8) {
+        /*int intensity = (Color.red(inPixel) + Color.green(inPixel) +
+                Color.blue(inPixel)) / 3;
+        return Color.argb(Color.alpha(inPixel), intensity,intensity,intensity);*/
+        return p4;
     }
 
 }
